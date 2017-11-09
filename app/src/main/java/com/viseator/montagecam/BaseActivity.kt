688cdb52f645de99b5971984b5c96022dc48d1d8
viseator <viseator@gmail.com>
2017-11-09 13:41:01 +0800

    fix fullscreen

--- app/src/main/java/com/viseator/montagecam/BaseActivity.kt
+++ app/src/main/java/com/viseator/montagecam/BaseActivity.kt
@@ -2,7 +2,8 @@ package com.viseator.montagecam
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import butterknife.ButterKnife
 
 /**
@@ -12,9 +13,9 @@ import butterknife.ButterKnife
  */
 
 abstract class BaseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(getLayout())
         ButterKnife.bind(this)
         init()
         initView()
@@ -22,14 +23,10 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun init()
     abstract fun initView()
-    abstract fun getLayout(): Int
     protected fun setFullScreen() {
-        val decorView: View = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
+
 }
--- app/src/main/java/com/viseator/montagecam/MainActivity.kt
+++ app/src/main/java/com/viseator/montagecam/MainActivity.kt
@@ -5,39 +5,39 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.view.Window
+import android.view.WindowManager
 import butterknife.BindView
 import com.google.android.cameraview.CameraView
 
 class MainActivity : BaseActivity() {
-
     private val REQUEST_CAMERA_PERMISSION: Int = 0x1;
 
     @BindView(R.id.main_camera_view)
     lateinit var mCameraView: CameraView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setFullScreen()
+        setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
-        mCameraView.start()
+
     }
 
     override fun onResume() {
         super.onResume()
-        when {
-            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED
-            -> ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission
-                    .CAMERA), REQUEST_CAMERA_PERMISSION)
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            mCameraView.start()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CAMERA_PERMISSION)
         }
     }
 
     override fun initView() {
-        setFullScreen()
     }
 
     override fun init() {
     }
 
-    override fun getLayout(): Int {
-        return R.layout.activity_main
-    }
 }
