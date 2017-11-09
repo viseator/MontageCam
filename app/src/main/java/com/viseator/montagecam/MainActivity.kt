f1dd9aea6ebefcc1efc8370ab7c8b60a5352806a
viseator <viseator@gmail.com>
2017-11-09 14:46:44 +0800

    add google presetted options default 16:9 ratio

--- app/src/main/java/com/viseator/montagecam/MainActivity.kt
+++ app/src/main/java/com/viseator/montagecam/MainActivity.kt
@@ -5,19 +5,35 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.view.Window
-import android.view.WindowManager
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import butterknife.BindView
+import com.google.android.cameraview.AspectRatio
 import com.google.android.cameraview.CameraView
 
-class MainActivity : BaseActivity() {
-    private val REQUEST_CAMERA_PERMISSION: Int = 0x1;
+class MainActivity : BaseActivity(), AspectRatioFragment.Listener {
+    val REQUEST_CAMERA_PERMISSION = 0x1
+    val TAG = "@vir MainActivity"
 
     @BindView(R.id.main_camera_view)
     lateinit var mCameraView: CameraView
+    @BindView(R.id.camera_toolbar)
+    lateinit var mToolBar: Toolbar
+    private val FLASH_OPTIONS = intArrayOf(CameraView.FLASH_AUTO, CameraView.FLASH_OFF,
+            CameraView.FLASH_ON)
+
+    private val FLASH_ICONS = intArrayOf(R.drawable.ic_flash_auto, R.drawable.ic_flash_off,
+            R.drawable.ic_flash_on)
+
+    private val FLASH_TITLES = intArrayOf(R.string.flash_auto, R.string.flash_off,
+            R.string.flash_on)
+    private val FRAGMENT_DIALOG = "dialog"
+    private var mCurrentFlash: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setFullScreen()
         setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
 
@@ -32,12 +48,68 @@ class MainActivity : BaseActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),
                     REQUEST_CAMERA_PERMISSION)
         }
+        val ratios = mCameraView.supportedAspectRatios
+        for (ratio in ratios) {
+            if (ratio.toString() == "16:9") {
+                mCameraView.setAspectRatio(ratio)
+                break
+            }
+        }
+    }
+
+    override fun onStop() {
+        mCameraView.stop()
+        super.onStop()
     }
 
     override fun initView() {
+        setSupportActionBar(mToolBar)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(false)
     }
 
     override fun init() {
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.aspect_ratio -> {
+                val fragmentManager = supportFragmentManager
+                if (fragmentManager.findFragmentByTag(
+                        FRAGMENT_DIALOG) == null) {
+                    val ratios = mCameraView.supportedAspectRatios
+                    val currentRatio = mCameraView.aspectRatio
+                    AspectRatioFragment.newInstance(ratios, currentRatio)
+                            .show(fragmentManager, FRAGMENT_DIALOG)
+                }
+                return true
+            }
+            R.id.switch_flash -> {
+                mCurrentFlash = (mCurrentFlash + 1) % FLASH_OPTIONS.size
+                item.setTitle(FLASH_TITLES[mCurrentFlash])
+                item.setIcon(FLASH_ICONS[mCurrentFlash])
+                mCameraView.flash = FLASH_OPTIONS[mCurrentFlash]
+                return true
+            }
+            R.id.switch_camera -> {
+                val facing = mCameraView.facing
+                mCameraView.facing = if (facing == CameraView.FACING_FRONT)
+                    CameraView.FACING_BACK
+                else
+                    CameraView.FACING_FRONT
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onAspectRatioSelected(ratio: AspectRatio) {
+        Toast.makeText(this, ratio.toString(), Toast.LENGTH_SHORT).show()
+        mCameraView.setAspectRatio(ratio)
+    }
 }
